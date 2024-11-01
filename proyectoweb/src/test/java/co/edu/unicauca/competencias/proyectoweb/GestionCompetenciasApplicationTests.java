13662bf4dfc0a7f07b64ae77ce969ae43669903b
Joan Sebastian Tuquerrez Gomez <jtuquerrez@unicauca.edu.co>
2024-11-01 00:06:03 -0500

    Started Spring Boot project

---
+++ proyectoweb/src/test/java/co/edu/unicauca/competencias/proyectoweb/GestionCompetenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.competencias.proyectoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCompetenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
