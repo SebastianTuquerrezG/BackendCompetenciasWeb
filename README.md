862313e48562bbe48cc93e9dc835b6f4b750f6b5
unknown <faciltroll2003@gmail.com>
2024-11-15 10:27:22 -0500

    Agregar Modulo Docente

---
+++ README.md
@@ -0,0 +1,2 @@
+# BackendCompetenciasWeb
+Repositorio donde se almacenara el proyecto para el backend del proyecto de la gestion de competencias para la universidad del cauca
---
+++ proyectoweb/src/main/java/co/edu/unicauca/competencias/proyectoweb/GestionCompetenciasApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.competencias.proyectoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCompetenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCompetenciasApplication.class, args);
+	}
+
+}
