a97af9314aef5a7f78959df5a166a0998c471522
Joan Sebastian Tuquerrez Gomez <jtuquerrez@unicauca.edu.co>
2024-12-03 01:20:09 -0500

    add  inserts for new tables

--- INSERTS.sql
+++ INSERTS.sql
@@ -6,14 +6,6 @@ INSERT INTO asignatura (nombre, creditos, semestre, objetivos, status, created_a
 ('Inteligencia Artificial', 5, 6, 'Fundamentos y aplicaciones de IA', 'ACTIVO', NOW(), NOW()),
 ('Seguridad Informática', 3, 5, 'Principios de seguridad y ciberseguridad', 'INACTIVO', NOW(), NOW());
 
-INSERT INTO competencia_asignatura (competencia_programa, descripcion, nivel_competencia, status, created_at, updated_at) VALUES
-(1, 'Competencia en análisis de datos', 'INTERMEDIO', 'ACTIVO', NOW(), NOW()),
-(2, 'Competencia en desarrollo de software', 'AVANZADO', 'ACTIVO', NOW(), NOW()),
-(3, 'Competencia en diseño de bases de datos', 'BASICO', 'ACTIVO', NOW(), NOW()),
-(4, 'Competencia en arquitectura de redes', 'INTERMEDIO', 'ACTIVO', NOW(), NOW()),
-(5, 'Competencia en inteligencia artificial', 'AVANZADO', 'ACTIVO', NOW(), NOW()),
-(6, 'Competencia en seguridad informática', 'INTERMEDIO', 'INACTIVO', NOW(), NOW());
-
 INSERT INTO competencias_programa (descripcion, nivel, estado) VALUES
 ('Competencia en Investigación Científica', 'AVANZADO', 1),
 ('Competencia en Desarrollo de Software', 'INTERMEDIO', 1),
@@ -21,6 +13,14 @@ INSERT INTO competencias_programa (descripcion, nivel, estado) VALUES
 ('Competencia en Análisis de Datos', 'AVANZADO', 1),
 ('Competencia en Innovación Tecnológica', 'INTERMEDIO', 1),
 ('Competencia en Emprendimiento', 'BASICO', 0);
+
+INSERT INTO competencia_asignatura (competencia_programa, descripcion, nivel_competencia, status, created_at, updated_at) VALUES
+(1, 'Competencia en análisis de datos', 'INTERMEDIO', 'ACTIVO', NOW(), NOW()),
+(2, 'Competencia en desarrollo de software', 'AVANZADO', 'ACTIVO', NOW(), NOW()),
+(3, 'Competencia en diseño de bases de datos', 'BASICO', 'ACTIVO', NOW(), NOW()),
+(4, 'Competencia en arquitectura de redes', 'INTERMEDIO', 'ACTIVO', NOW(), NOW()),
+(5, 'Competencia en inteligencia artificial', 'AVANZADO', 'ACTIVO', NOW(), NOW()),
+(6, 'Competencia en seguridad informática', 'INTERMEDIO', 'INACTIVO', NOW(), NOW());
 
 INSERT INTO ra_programa (id_competencia_programa, descripcion, estado) VALUES
 (1, 'Resultado de programa en investigación científica avanzada', 1),
@@ -53,3 +53,22 @@ INSERT INTO usuarios (username, email, `nombre(s)`, `apellido(s)`, identificacio
 ('ana.garcia', 		'ana.garcia@example.com', 'Ana', 'García', 'XY123456', 'TARJETA_EXTRANJERA', 'cloudpass101', '3187654321', 'Doctora', 'DOCENTE', 'ACTIVO', NOW(), NOW(), 1, 0),
 ('pedro.sanchez', 	'pedro.sanchez@example.com', 	'Pedro', 	'Sánchez', '5555555555', 'CEDULA_CIUDADANIA', 'airesearch202', '3134567890', 'Doctor', 'EVALUADOR', 'ACTIVO', NOW(), NOW(), 1, 0),
 ('laura.fernandez', 'laura.fernandez@example.com', 'Laura', 	'Fernández', 'CD789012', 'TARJETA_DE_IDENTIDAD', 'systemspass303', '3112345678', 'Ingeniera', 'ADMIN', 'INACTIVO', NOW(), NOW(), 0, 3);
+
+
+INSERT INTO RUBRICA (nombre, ra_asignatura_id, create_At, update_At, estado)
+VALUES ('Rúbrica de Programación', 1, NOW(), NOW(), "ACTIVO"),
+('Rúbrica de Algoritmos', 2, NOW(), NOW(), "ACTIVO");
+
+INSERT INTO criterio_evaluacion (descripcion, ponderacion, id_rubrica, estado)
+VALUES ('Calidad del código', 40, 1, "ACTIVO"),
+('Eficiencia del algoritmo', 30, 1, "ACTIVO"),
+( 'Documentación', 30, 2, "ACTIVO");
+
+INSERT INTO nivel_desempenio (criterio_evaluacion_id, nombre, descripcion, rango_Nota)
+VALUES (1, 'EXCELENTE', 'Código limpio, modular y sin errores', '4.5-5.0'),
+(2, 'BUENO', 'Algoritmo eficiente, pero con posibles mejoras', '3.8-4.49'),
+(3, 'REGULAR', 'Documentación suficiente, pero no exhaustiva', '2.5-3.79');
+
+
+
+
